d11bb0e64fbe6eb4d2da52bcb614abc15b0734ae
Thleighton <165794617+Thleighton@users.noreply.github.com>
2024-09-20 23:02:43 -0300

    lo que se pudo

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -12,123 +12,104 @@ import jsQR, { QRCode } from 'jsqr';
   templateUrl: 'inicio.page.html',
   styleUrls: ['inicio.page.scss'],
 })
-
-
-
 export class InicioPage implements OnInit {
-miclase() {
-throw new Error('Method not implemented.');
-}
-misdatos() {
-throw new Error('Method not implemented.');
-}
-inicio() {
-throw new Error('Method not implemented.');
-}
+  miclase() {
+    throw new Error('Method not implemented.');
+  }
+  misdatos() {
+    throw new Error('Method not implemented.');
+  }
+  inicio() {
+    throw new Error('Method not implemented.');
+  }
 
-@ViewChild('video')
-private video!: ElementRef;
+  @ViewChild('video')
+  private video!: ElementRef;
 
-@ViewChild('canvas')
-private canvas!: ElementRef;
-
+  @ViewChild('canvas')
+  private canvas!: ElementRef;
 
   public usuario: Usuario = new Usuario('', '', '', '', '');
-
   public nivelesEducacionales: NivelEducacional[] = new NivelEducacional().getNivelesEducacionales();
-
   public persona: Persona = new Persona();
-
   public asistencia: Asistencia = new Asistencia();
-
   public escaneando = false;
-
   public datosQR: string = '';
 
+  constructor(
+    private activeroute: ActivatedRoute,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
- 
-  /*
-    En el constructor del HomePage se ponen como parametros los siguientes objetos:
-      (1) activeroute (del tipo de dato ActivatedRoute) y router (del tipo de dato Router),
-      que se usarán para obtener los datos enviados por la página que invocó a "home".
-      (2) alertController (del tipo de dato AlertController), que se usará para mostrar
-      mensajes emergentes en la pantalla.
-
-    Nótese que los parámetros tuvieron que declararse con "private", y esto es necesario
-    para que los parámetros pasen a considerarse automáticamente como propiedades
-    de la clase "HomePage" y de este modo puedan usarse dentro de los otros métodos.
-   */
-   constructor(
-        private activeroute: ActivatedRoute
-      , private router: Router
-      , private alertController: AlertController) {
-}
-
-public ngOnInit() {
-
-   // Aquí obtenemos los datos del usuario que fue enviado desde la página de login
-   const navExtras = this.router.getCurrentNavigation()?.extras?.state;
-   if (navExtras && navExtras['usuario']) {
-     this.usuario = navExtras['usuario'];
-
-}
-}
-
-public async comenzarEscaneoQR() {
-  const mediaProvider: MediaProvider = await navigator.mediaDevices.getUserMedia({
-    video: {facingMode: 'environment'}
-  });
-  this.video.nativeElement.srcObject = mediaProvider;
-  this.video.nativeElement.setAttribute('playsinline', 'true');
-  this.video.nativeElement.play();
-  this.escaneando = true;
-  requestAnimationFrame(this.verificarVideo.bind(this));
-}
-
-async verificarVideo() {
-  if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
-    if (this.obtenerDatosQR() || !this.escaneando) return;
-    requestAnimationFrame(this.verificarVideo.bind(this));
-  } else {
-    requestAnimationFrame(this.verificarVideo.bind(this));
-  }
-}
-
-
-public obtenerDatosQR(): boolean {
-  const w: number = this.video.nativeElement.videoWidth;
-  const h: number = this.video.nativeElement.videoHeight;
-  this.canvas.nativeElement.width = w;
-  this.canvas.nativeElement.height = h;
-  const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
-  context.drawImage(this.video.nativeElement, 0, 0, w, h);
-  const img: ImageData = context.getImageData(0, 0, w, h);
-  let qrCode: QRCode | null = jsQR(img.data, w, h, { inversionAttempts: 'dontInvert' });
-  if (qrCode) {
-    if (qrCode.data !== '') {
-      this.escaneando = false;
-      this.mostrarDatosQROrdenados(qrCode.data);
-      return true;
+  public ngOnInit() {
+    // Aquí obtenemos los datos del usuario que fue enviado desde la página de login
+    const navExtras = this.router.getCurrentNavigation()?.extras?.state;
+    if (navExtras && navExtras['usuario']) {
+      this.usuario = navExtras['usuario'];
     }
   }
-  return false;
-}
 
-public mostrarDatosQROrdenados(datosQR: string): void {
-  // Parseamos los datos del QR
-  const objetoDatosQR = JSON.parse(datosQR);
+  // Función para navegar a la página de Mis Datos
+  public irAMisDatos() {
+    this.router.navigate(['/mis-datos'], {
+      state: {
+        usuario: this.usuario, // Pasar usuario a la siguiente página
+      },
+    });
+  }
 
-  // Asignamos los datos del QR a la propiedad 'datosQR'
-  this.datosQR = datosQR;
+  public async comenzarEscaneoQR() {
+    const mediaProvider: MediaProvider = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' },
+    });
+    this.video.nativeElement.srcObject = mediaProvider;
+    this.video.nativeElement.setAttribute('playsinline', 'true');
+    this.video.nativeElement.play();
+    this.escaneando = true;
+    requestAnimationFrame(this.verificarVideo.bind(this));
+  }
 
-  // Navegamos a la página 'miclase' pasando los datos como parámetros
-  this.router.navigate(['/miclase'], {
-    state: { datosClase: objetoDatosQR }
-  });
-}
+  async verificarVideo() {
+    if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
+      if (this.obtenerDatosQR() || !this.escaneando) return;
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    } else {
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    }
+  }
 
+  public obtenerDatosQR(): boolean {
+    const w: number = this.video.nativeElement.videoWidth;
+    const h: number = this.video.nativeElement.videoHeight;
+    this.canvas.nativeElement.width = w;
+    this.canvas.nativeElement.height = h;
+    const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
+    context.drawImage(this.video.nativeElement, 0, 0, w, h);
+    const img: ImageData = context.getImageData(0, 0, w, h);
+    let qrCode: QRCode | null = jsQR(img.data, w, h, { inversionAttempts: 'dontInvert' });
+    if (qrCode) {
+      if (qrCode.data !== '') {
+        this.escaneando = false;
+        this.mostrarDatosQROrdenados(qrCode.data);
+        return true;
+      }
+    }
+    return false;
+  }
 
- public detenerEscaneoQR(): void {
+  public mostrarDatosQROrdenados(datosQR: string): void {
+    // Parseamos los datos del QR
+    const objetoDatosQR = JSON.parse(datosQR);
+    // Asignamos los datos del QR a la propiedad 'datosQR'
+    this.datosQR = datosQR;
+    // Navegamos a la página 'miclase' pasando los datos como parámetros
+    this.router.navigate(['/miclase'], {
+      state: { datosClase: objetoDatosQR },
+    });
+  }
+
+  public detenerEscaneoQR(): void {
     this.escaneando = false;
   }
 
@@ -136,6 +117,4 @@ public mostrarDatosQROrdenados(datosQR: string): void {
     // Navegamos a la página de login
     this.router.navigate(['/login']);
   }
-
-
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -12,30 +12,13 @@ export class LoginPage implements OnInit {
 
   public placeholderCorreo: string = '';  
   public placeholderPassword: string = '';  
-  /*
-    Se genera el modelo user con dos claves (key), las que se comportan como propiedades
-    de la clase LoginPage.
-    En Modelo MVC, la clase "LoginPage" viene siendo el "Controlador", encargado de administrar
-    la parte gráfica de la página web de login, por lo que trabaja coordinado con el
-    archivo login.page.html
-    Cada propiedad tiene su valor inicial y tiene su pareja de control HTML que es el <ion-input>
-    De este modo el TAG:
-    <ion-input type="text" [(ngModel)]="login.Usuario"></ion-input>
-    ya sabe que tiene que trabajar con el modelo de Angular a través de la
-    propiedad "login.Usuario".
-    Fijarse que si se le colocan valores iniciales a login.usuario y login.password,
-    estos aparecerán inmediatamente reflejados en la página login cuando esta
-    se abra por primera vez.
-  */
+
+  
 
   public usuario: Usuario;
-  /*
-    Para poder trabajar con Router y poder navegar hacia la página "home", debemos primero
-    pasar como parámetro e instanciar un objeto de la clase "Router". Fijarse que el tipo
-    de dato, que se pasa en el constructor es "Router" con mayúscula, porque se trata de
-    una clase y éstas parten con letra mayúscula, mientras que "router" con minúscula es
-    el objeto de esa clase, que usaremos para ejecutar el método "navigate".
-  */
+
+
+
   constructor(private router: Router, private toastController: ToastController) {
     this.usuario = new Usuario('', '', '', '', '');
     this.usuario.correo = '';
@@ -44,19 +27,11 @@ export class LoginPage implements OnInit {
 
   public ngOnInit(): void {
 
-    /*
-      Las siguientes 3 líneas de código sirven para lo siguiente:
-        Caso 1: Si las comentas, la página quedará lista para ingresar el nombre de
-          usuario y la password
-        Caso 2: Si dejas las instrucciones sin comentar, entonces entrará inmediatamente
-          a la página home, usando el usuario por defecto "cgomezvega" con la
-          password "123". Lo anterior es muy útil para el caso en que ya quedó lista
-          la página de login y me interesa probar las otras páginas, de este modo se saltará
-          el login y no tendrás que estar digitando los datos todo el tiempo.
-    */
+
+ 
     this.usuario.correo = '';
     this.usuario.password = '';
-    //this.ingresar();
+    
   }
 
  
